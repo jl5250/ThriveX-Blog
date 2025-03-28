a7ab8cbc0312b3f79bfaf82e8aaa4fec1218923e
jl5250 <1662528926@qq.com>
2025-03-29 01:11:17 +0800

    feat: 写了音乐相册架构

---
+++ src/app/album/page.tsx
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <div className="bg-white">
+      <div className="mt-24 mx-auto flex flex-col items-center">
+        <h1 className="w-6/12 text-[2vw] text-[#888] font-medium mt-8 text-xl">
+          音乐相册页面建设中。。。 稍等博主稳定后再建设
+        </h1>
+      </div>
+    </div>
+  )
+}
