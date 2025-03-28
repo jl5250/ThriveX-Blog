41f7dde39fd49c421ba46fca408dda425e84bb68
jl5250 <1662528926@qq.com>
2025-03-28 23:47:38 +0800

    style: 修改百度统计的代码

--- src/components/BaiduStatis/index.tsx
+++ src/components/BaiduStatis/index.tsx
@@ -1,19 +1,21 @@
-"use client"
+'use client'
 
-import Script from "next/script"
+import Script from 'next/script'
 
 export default () => {
-    return (
-        <Script dangerouslySetInnerHTML={{
-            __html: `
-              var _hmt = _hmt || [];
-              (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?e5bf799a3e49312141c8b677b7bec1c2";
-                var s = document.getElementsByTagName("script")[0]; 
-                s.parentNode.insertBefore(hm, s);
-              })();
+  return (
+    <Script
+      dangerouslySetInnerHTML={{
+        __html: `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?7bc7834cf1035acce8b851c0b9c95fd3";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
             `
-        }} />
-    )
+      }}
+    />
+  )
 }
