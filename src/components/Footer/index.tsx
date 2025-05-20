059cc8c6c0d9a76ebe8a62b7cdf99afc6b5ab492
jl5250 <1662528926@qq.com>
2025-05-20 16:13:57 +0800

    feat: 添加备案号！

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,42 +1,76 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { getConfigDataAPI } from '@/api/project';
-import { getUserDataAPI } from '@/api/user';
-import { User } from '@/types/app/user';
-import { Web } from '@/types/app/project';
-import Tooltip from './components/Tooltip';
+import Link from 'next/link'
+import Image from 'next/image'
+import { getConfigDataAPI } from '@/api/project'
+import { getUserDataAPI } from '@/api/user'
+import { User } from '@/types/app/user'
+import { Web } from '@/types/app/project'
+import Tooltip from './components/Tooltip'
 
-import animals from './images/animals.webp';
+import animals from './images/animals.webp'
 
 export default async () => {
   const { data: user } = (await getUserDataAPI()) || { data: {} as User }
-  const { data: web } = (await getConfigDataAPI<Web>("web")) || { data: {} as Web }
+  const { data: web } = (await getConfigDataAPI<Web>('web')) || { data: {} as Web }
 
   return (
     <>
       <div className='sticky bottom-0 z-30 translate-y-[25px] flex justify-center w-full bg-cover bg-center after:content-[""] after:w-full after:h-[60%] after:absolute after:bottom-[25px] after:left-0 after:bg-[linear-gradient(to_top,#fff,transparent)] dark:after:bg-[linear-gradient(to_top,#2c333e,transparent)]'>
-        <div className='flex justify-center lg:w-[950px] xl:w-[1200px] mx-auto'>
-          <Image src={animals} alt="动物" width={660.34} height={79.99} className='relative z-40 hidden md:block' />
+        <div className="flex justify-center lg:w-[950px] xl:w-[1200px] mx-auto">
+          <Image
+            src={animals}
+            alt="动物"
+            width={660.34}
+            height={79.99}
+            className="relative z-40 hidden md:block"
+          />
         </div>
       </div>
 
-      <div className='absolute z-10 w-full bg-white dark:bg-black-b border-t dark:border-black-b px-10 transition-colors'>
+      <div className="absolute z-10 w-full bg-white dark:bg-black-b border-t dark:border-black-b px-10 transition-colors">
         <div className="flex justify-center items-center py-4">
-          <img src={user?.avatar} alt='作者头像' className='w-20 h-20 rounded-full mr-8 avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.1)]' />
-          <h2 className="w-[90%] xl:w-3/6 text-sm sm:text-base dark:text-[#8c9ab1] line-clamp-4">{web?.footer}</h2>
+          <img
+            src={user?.avatar}
+            alt="作者头像"
+            className="w-20 h-20 rounded-full mr-8 avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.1)]"
+          />
+          <h2 className="w-[90%] xl:w-3/6 text-sm sm:text-base dark:text-[#8c9ab1] line-clamp-4">
+            {web?.footer}
+          </h2>
         </div>
 
         {/* 
             为了项目的生态越来越强大，作者在这里恳请大家保留 ThriveX 博客系统版权
             在项目 Star 突破 2K 后大家可自由选择删除 or 保留版权
         */}
-        <div className='py-4 border-t dark:border-black-a transition-colors'>
+        <div className="py-4 border-t dark:border-black-a transition-colors">
           <Tooltip content="一款免费、开源、年轻、高颜值的现代化博客管理系统">
-            <div className='flex justify-center items-center space-x-3'>
-              <img src="https://bu.dusays.com/2024/11/17/6739adf188f64.png" width={30} height={30} alt='ThriveX 博客管理系统' />
-              <Link href="https://github.com/LiuYuYang01/ThriveX-Admin" target='_blank' className='hover:text-primary transition-colors'> 基于开源项目 ThriveX 构建</Link>
+            <div className="flex justify-center items-center space-x-3">
+              <img
+                src="https://bu.dusays.com/2024/11/17/6739adf188f64.png"
+                width={30}
+                height={30}
+                alt="ThriveX 博客管理系统"
+              />
+              <Link
+                href="https://github.com/LiuYuYang01/ThriveX-Admin"
+                target="_blank"
+                className="hover:text-primary transition-colors"
+              >
+                基于开源项目 ThriveX 构建
+              </Link>
             </div>
           </Tooltip>
+        </div>
+        <div className="text-center text-xs text-gray-400 py-2">
+          © {new Date().getFullYear()} loong极客笔记 保留所有权利
+          <a
+            href="https://beian.miit.gov.cn/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-2 underline"
+          >
+            粤ICP备2025392562号-1
+          </a>
         </div>
       </div>
     </>
