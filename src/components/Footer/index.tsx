37cca0e6fad955676055fd31c2249e36b77d66a1
jl5250 <1662528926@qq.com>
2025-05-26 13:54:57 +0800

    fix: 备案号跳转到工信部！

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -41,7 +41,13 @@ export default async () => {
 
         <div className="group flex justify-center space-x-2 pb-4 cursor-pointer">
           <Image src={ICP} alt="ICP" width={20} height={22} className="w-5 h-[22px]" />
-          <span className="group-hover:text-primary transition-colors">{web.icp}</span>
+          <a
+            className="group-hover:text-primary transition-colors"
+            href="https://beian.miit.gov.cn/"
+            target="_blank"
+          >
+            {web.icp}
+          </a>
         </div>
 
         {/* 
