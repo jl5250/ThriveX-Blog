5aa383d8bbb323a6a44ac200cf825749b624cdd1
宇阳 <liuyuyang1024@yeah.net>
2026-02-13 15:39:47 +0800

    style(eslint): 调整 typescript-eslint 规则并移除重复配置
    移除重复的 '@typescript-eslint/no-explicit-any' 规则配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig([
   nextPlugin.configs.recommended,
   {
     rules: {
+      // 禁止使用 any 类型
       '@typescript-eslint/no-explicit-any': 'warn', // 改为警告，鼓励使用具体类型
       'no-unused-vars': 'warn', // 改为警告，提醒未使用的变量
       'react-refresh/only-export-components': 'off',
@@ -40,8 +41,6 @@ export default defineConfig([
       'react/react-in-jsx-scope': 'off',
       // 约束使用 next/image 组件
       '@next/next/no-img-element': 'off',
-      // 禁止使用 any 类型
-      '@typescript-eslint/no-explicit-any': 'warn'
     },
   },
 ]);
