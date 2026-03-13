30b386b7b35b90e470cdfd7247b2503fb8eb4938
jl5250 <1662528926@qq.com>
2026-03-13 23:25:03 +0800

    优化主页个人卡片代码

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,25 +1,44 @@
-import Author from './Author';
-import HotArticle from './HotArticle';
-import RandomArticle from './RandomArticle';
-import Comment from './Comment';
-import RunTime from './RunTime';
-import Study from './Study';
-import { getWebConfigDataAPI } from '@/api/config';
-import { Theme } from '@/types/app/config';
-import { getAuthorDataAPI } from '@/api/user';
-import { User } from '@/types/app/user';
+import Author from './Author'
+import HotArticle from './HotArticle'
+import RandomArticle from './RandomArticle'
+import Comment from './Comment'
+import RunTime from './RunTime'
+import Study from './Study'
+import { getWebConfigDataAPI } from '@/api/config'
+import { Theme } from '@/types/app/config'
+import { getAuthorDataAPI } from '@/api/user'
+import { User } from '@/types/app/user'
 
 export default async () => {
-  const themeResponse = await getWebConfigDataAPI<{ value: Theme }>('theme');
-  const theme = themeResponse?.data?.value || ({} as Theme);
-  const sidebar = theme?.right_sidebar || [];
-  const { data: user } = (await getAuthorDataAPI()) || { data: {} as User };
+  const themeResponse = await getWebConfigDataAPI<{ value: Theme }>('theme')
+  const theme = themeResponse?.data?.value || ({} as Theme)
+  const sidebar = theme?.right_sidebar || []
+  const { data: user } = (await getAuthorDataAPI()) || { data: {} as User }
 
   return (
     <>
-      <div className={`hidden lg:block ${sidebar.length ? 'lg:w-[29%] xl:w-[24%]' : 'w-0'} rounded-md transition-width sticky top-[70px]`}>
+      <div
+        className={`hidden lg:block ${sidebar.length ? 'lg:w-[29%] xl:w-[24%]' : 'w-0'} rounded-md transition-width sticky top-[70px]`}
+      >
         {/* 作者介绍 */}
-        {sidebar.includes('author') && <Author theme={theme} name={user?.name} title={user?.info} handle="javicodes" status="Online" contactText="Contact Me" avatarUrl={user?.avatar} showUserInfo enableTilt={true} enableMobileTilt behindGlowColor="rgba(125, 190, 255, 0.67)" iconUrl={user?.avatar} behindGlowEnabled innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)" />}
+        {sidebar.includes('author') && (
+          <Author
+            theme={theme}
+            name={user?.name}
+            title={user?.info}
+            handle="javicodes"
+            status="Online"
+            contactText="Contact Me"
+            avatarUrl={user?.avatar}
+            showUserInfo
+            enableTilt={true}
+            enableMobileTilt={false}
+            behindGlowColor="rgba(125, 190, 255, 0.67)"
+            iconUrl={user?.avatar}
+            behindGlowEnabled
+            innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
+          />
+        )}
         {/* 站点已运行 */}
         {sidebar.includes('runTime') && <RunTime />}
         {/* 随机推荐 */}
@@ -32,5 +51,5 @@ export default async () => {
         {sidebar.includes('study') && <Study />}
       </div>
     </>
-  );
-};
+  )
+}
