57c164560b40da6de51976ff277faf89356bb31b
宇阳 <liuyuyang1024@yeah.net>
2026-03-15 12:21:24 +0800

    重构（api）：更新文章和分类 API 方法，使用 GET 请求
    - 将文章列表和分页 API 方法从 POST 改为 GET，以提高清晰度和性能。
    - 更新了 RSS 路由和 ArticleLayout 组件中的 API 调用结构，以反映新的方法签名。

--- src/api/cate.ts
+++ src/api/cate.ts
@@ -4,7 +4,7 @@ import Request from '@/utils/request'
 
 // 获取分类列表
 export const getCateListAPI = async () => {
-    return await Request<Cate[]>('POST', '/cate/list')
+    return await Request<Cate[]>('GET', '/cate?pattern=tree')
 }
 
 // 获取指定分类中的所有文章
